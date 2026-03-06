93736d46630c8c8e66623295247d8abd6fd8c383
MukhamedovR <255413916+MukhamedovR@users.noreply.github.com>
2026-03-06 15:53:21 +0300

    Update UserDao.java

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -5,13 +5,15 @@ import entity.User;
 import java.util.List;
 
 public interface UserDao {
+
     void save(User user);
 
     User getById(Long id);
+
+    void update(User user);
 
     void delete(Long id);
 
     List<User> getAll();
 }
-
 
